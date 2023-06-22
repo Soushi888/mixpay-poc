2ab6dc413e5d192c9702e763a11d6658f137c13c
soushi888 <sacha.pignot@protonmail.com>
2023-06-22 19:20:26 -0400

    adjust footer text

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,7 +1,4 @@
 <div class="h-20 flex justify-center items-center gap-4 text-3xl mb-4">
-	Powered by <a href="https://mixpay.me/">MixPay</a><img
-		src="mixpay-logo.png"
-		alt="Mixpay Logo"
-		class="h-20"
-	/>
+	<p>Powered by <a href="https://mixpay.me/" class="underline">MixPay</a></p>
+	<img src="mixpay-logo.png" alt="Mixpay Logo" class="h-20" />
 </div>
