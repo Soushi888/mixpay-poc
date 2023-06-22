b0fd6f11c8f36cd30175c5f0e8fc8fd6f2f87ace
soushi888 <sacha.pignot@protonmail.com>
2023-06-22 19:22:17 -0400

    rounded footer logo

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,4 +1,4 @@
 <div class="h-20 flex justify-center items-center gap-4 text-3xl mb-4">
 	<p>Powered by <a href="https://mixpay.me/" class="underline">MixPay</a></p>
-	<img src="mixpay-logo.png" alt="Mixpay Logo" class="h-20" />
+	<img src="mixpay-logo.png" alt="Mixpay Logo" class="h-20 rounded-lg" />
 </div>
