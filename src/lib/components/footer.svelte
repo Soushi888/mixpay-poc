985562711cd9733a7245c04a507297eba011cb20
soushi888 <sacha.pignot@protonmail.com>
2023-06-22 20:07:18 -0400

    correct footer height and alignement

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,3 +1,3 @@
-<div class="h-10 flex justify-center items-center gap-4 text-xl mb-4">
+<div class="h-5 flex justify-center items-center text-xl m-4">
 	<p>Powered by <a href="https://mixpay.me/" class="underline">MixPay</a></p>
 </div>
